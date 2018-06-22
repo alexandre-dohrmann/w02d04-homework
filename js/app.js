af2f76c3f1aab859168c0351da62e52bb103014c
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-06-22 16:39:35 -0600

    Commit 5 - The days go by slowly and the years go by too fast

--- js/app.js
+++ js/app.js
@@ -6,34 +6,64 @@
 //   console.log('I did it! I linked jQuery and this js file!')
 // };
 
-const $container = $('#container');
-console.log($container);
+// -----------
+// YEAR 1:
+// -----------
 
-const $h1 = $('h1');
-$container.append($h1);
-$container.append('<h1>Hogwarts</h1>');
+	const $container = $('#container');
+	console.log($container);
 
-const $h2 = $('h2');
-$container.append($h2);
-$container.append('<h2>Alexandre</h2>');
+	const $h1 = $('h1');
+	$container.append($h1);
+	$container.append('<h1>Hogwarts</h1>');
 
-const $h3 = $('h3');
-$container.append($h3);
-$container.append('<h3>Gryffindor</h3>');
+// -----------
+// YEAR 2:
+// -----------
 
-const $h4 = $('h4');
-$container.append($h4);
-$container.append('<h4 class="cat">Midori</h4>');
-$container.append('<h4>Holly Wand with Unicorn Hair Core</h4>');
+	const $h2 = $('h2');
+	$container.append($h2);
+	$container.append('<h2>Alexandre</h2>');
 
-const $ul = $('<ul storage="trunk"></ul>');
-$container.append('<li>butter beer</li>');
-$container.append('<li class="secret">invisibility cloak</li>');
-$container.append('<li class="secret">magic map</li>');
-$container.append('<li class="secret">time turner</li>');
-$container.append('<li class="cat">leash</li>');
-$container.append('<li>Bertie Bott\'s Every Flavor [Jelly] Beans</li>');
+	const $h3 = $('h3');
+	$container.append($h3);
+	$container.append('<h3>Gryffindor</h3>');
 
+	const $h4 = $('h4');
+	$container.append($h4);
+	$container.append('<h4 class="cat">Midori</h4>');
+	$container.append('<h4 class="wand">Holly Wand with Unicorn Hair Core</h4>');
 
+// -----------
+// YEAR 3:
+// -----------
 
+	const $ul = $('<ul storage="trunk"></ul>');
+	$container.append('<li>butter beer</li>');
+	$container.append('<li class="secret">invisibility cloak</li>');
+	$container.append('<li class="secret">magic map</li>');
+	$container.append('<li class="secret">time turner</li>');
+	$container.append('<li class="cat">leash</li>');
+	$container.append('<li>Bertie Bott\'s Every Flavor [Jelly] Beans</li>');
+
+// -----------
+// YEAR 4:
+// -----------
+
+	const $h5 = $('h5');
+	$container.append('<h5>Spring 2018</h5>');
+
+	const $table = $('<table></table>');
+	$container.append('<table><thead><tr><th>Day</th><th>Classes</th><tr><td>Monday</td><td>Herbology, Divination, History of Magic</td></tr><tr><td>Tuesday</td><td>Transfiguration, Defense Against the Dark Arts, Quidditch Practice</td></tr><tr><td>Wednesday</td><td>Herbology, Divination, History of Magic</td></tr><tr><td>Thursday</td><td>Transfiguration, Defense Against the Dark Arts, Quidditch Practice</td></tr><tr><td>Friday</td><td>Herbology, Divination, History of Magic</td></tr></thead></table>')
+
+// -----------
+// YEAR 5:
+// -----------
+
+	$('h4.wand').remove();	
+	$('li').eq(0).remove();
+	$('h4').after($('<h4>').text('Hornbeam Wand with Dragon Heartstring').css('color', 'indigo'));
+	$('h4.cat').eq(0).replaceWith('<h4 class="cat">Midori (spy mission)</h4>');
+	$('h4.cat').appendTo($('body'));
+	$('h4').eq(0).before($('h4.cat'));
 
