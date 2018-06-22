c04a4e54f3e2e351cedb7d39b5acc861b9e5ff24
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-06-21 23:10:45 -0600

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- js/app.js
+++ js/app.js
@@ -13,3 +13,14 @@ const $h1 = $('h1');
 $container.append($h1);
 $container.append('<h1>Hogwarts</h1>');
 
+const $h2 = $('h2');
+$container.append($h2);
+$container.append('<h2>Alexandre</h2>');
+
+const $h3 = $('h3');
+$container.append($h3);
+$container.append('<h3>Gryffindor</h3>');
+
+const $h4 = $('h4');
+$container.append($h4);
+$container.append('<h4>Holly Wand with Unicorn Hair Core</h4>');
